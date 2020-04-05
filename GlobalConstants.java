2ac78a7829883e8b41ed2a4c8fdc9c272d1947fe
quickdra <rajatvpanchal@gmail.com>
2020-04-05 13:07:14 +0530

    Create GlobalConstants.java

---
+++ GlobalConstants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vlpap
+ */
+public interface GlobalConstants {
+    // Random constants, change to the true values later
+    double PplPerM2 = 0.05;
+    int MaxTimeInStore = 20; // in Minutes
+    int MaxWindow = 5; // in Hours?
+    
+    
+}
